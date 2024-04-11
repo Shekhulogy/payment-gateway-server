4cd6018fd0edbf221aa46b3b07d7923aab78cc08
Anurag Patel <anuragpatel.optnio@gmail.com>
2024-04-11 11:04:49 +0530

    initial setup

---
+++ api/payment/checkout.ts
@@ -0,0 +1,31 @@
+import type { VercelRequest, VercelResponse } from "@vercel/node";
+import Razorpay from "razorpay";
+
+const instance = new Razorpay({
+  key_id: process.env.RAZORPAY_KEY_ID || "rzp_test_Oqm7HBK9mo9m4e",
+  key_secret: process.env.RAZORPAY_KEY_SECRET || "LRoBW7qiwQZ7jPxOffx2F1Gp",
+});
+
+export default async function handler(req: VercelRequest, res: VercelResponse) {
+  if (req.method === "POST") {
+    const { amount = 0 } = req.body;
+    if (!amount) {
+      return res.json({
+        success: false,
+      });
+    }
+    const options = {
+      amount: Math.floor(amount * 100),
+      currency: "INR",
+    };
+    const order = await instance.orders.create(options);
+
+    return res.json({
+      success: true,
+      order,
+    });
+  }
+  return res
+    .status(400)
+    .json({ message: `Incorrect method: ${req.method}. Did you mean POST?` });
+}
