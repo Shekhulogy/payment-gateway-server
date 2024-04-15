b3d948a64d4111c899d2285b3d10d3a7203da73c
shekhulogy <shikhar.patel.96@gmail.com>
2024-04-15 22:25:52 +0530

    added new file

--- api/key.ts
+++ api/key.ts
@@ -1,8 +1,9 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node";
 
 export default function handler(req: VercelRequest, res: VercelResponse) {
-  const { name = "vercel" } = req.query;
+  const { key_id = process.env.RAZORPAY_KEY_ID || "rzp_test_Oqm7HBK9mo9m4e" } =
+    req.query;
   return res.json({
-    message: `server running on ${name}!`,
+    key_id,
   });
 }
